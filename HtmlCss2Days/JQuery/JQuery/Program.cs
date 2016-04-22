c3b89c7b381422bbd6cf6ec6dda630d6e36bf47a
ccristiand <Cristian.Constantin@endava.com>
2016-04-22 12:20:19 +0300

    Added files via upload

---
+++ HtmlCss2Days/JQuery/JQuery/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
